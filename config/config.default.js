0f1e13a6b24ff4f53013622c45b8909595e07def
zhouzhihu <web_study2004@163.com>
2017-05-24 10:38:06 +0800

    增加自定义异常处理

--- config/config.default.js
+++ config/config.default.js
@@ -1,10 +1,13 @@
 exports.keys = "ssss";
-exports.middleware = [ 'cors' ];
+exports.middleware = [ 'cors', 'errorHandler' ];
 exports.cors = {
   origin: 'http://10.1.10.16:8080',
   maxAge: 1000000,
   credentials: true
 };
+exports.errorHandler = {
+  match: '/api'
+}
 exports.api = {
   egrand_api: 'http://10.1.10.205:8083'
 }
